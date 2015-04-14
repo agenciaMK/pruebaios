13b9b0a7e941f9f8b2f0255598fca158f911e5a1
Javier Diez Briz <jdiez@mkmedia.es>
2015-04-14 11:12:40 +0200

    uno

---
+++ config.js
@@ -0,0 +1,17 @@
+define( function ( require ) {
+
+	"use strict";
+
+	return {
+		app_slug : 'prueba-para-ios',
+		wp_ws_url : 'http://dev.mkmedia.es/wp-appkit-api/prueba-para-ios',
+		theme : 'nihao-ios',
+		version : '1.0.0',
+		app_title : 'Prueba iOS',
+		debug_mode : 'on',
+		auth_key : '66X `sB-5fs6c?Xss =gc`)xCq|=s%*bR?xVd[Dx0pbd@~B.7;kPxtE(GLJGFayn',
+		options : {"refresh_interval":0},
+		addons : []
+	};
+
+});
